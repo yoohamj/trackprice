489ea65160fe5ebdc8596fe7a345cb5f1f860a85
Yooham Jung <yoohamj@gmail.com>
2024-08-07 00:12:42 -0400

    setup local env using amplify_outputs.json

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,10 @@ export default function App() {
       content: window.prompt("Todo content"),
     });
   }
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
 
   return (
     <main>
@@ -37,7 +41,10 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+          </li>
         ))}
       </ul>
       <div>
