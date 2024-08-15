8b50596bf4c274b758b9cbd3fdcacf3a4077661b
Yooham Jung <yoohamj@gmail.com>
2024-08-15 01:54:33 -0400

    search bar implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./app.css";
+import './globals.css';
+import { Navbar } from 'components/layout/navbar';
+import { GeistSans } from 'geist/font/sans';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={GeistSans.variable}>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+          <Navbar />
+          <main>
+          </main>
+      </body>
     </html>
   );
 }
--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
@@ -11,17 +14,24 @@ Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export const metadata = {
-  description: 'Price Tracking website built with Next.js.',
-    openGraph: {
-      type: 'website'
-    }
-  };
-
 export default function App() {
+
   return (
+    <Authenticator>
+      {({ signOut, user }) => (
         <main>
+          <h1>My todos</h1>
+          <div>
+            🥳 App successfully hosted. Try creating a new todo.
+            <br />
+            <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+              Review next steps of this tutorial.
+            </a>
+          </div>
+          <button onClick={signOut}>Sign out</button>
         </main>
-      )
+      )}
+    </Authenticator>
+  )
 }
 
