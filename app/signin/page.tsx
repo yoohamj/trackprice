e7805fdf30448f960dbfcdb0a0cfcd2b850e398f
Yooham Jung <yoohamj@gmail.com>
2024-08-15 02:11:12 -0400

    removed app.css references

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,7 +5,6 @@ import '@aws-amplify/ui-react/styles.css'
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
